edc722c745885de7c66e91f207af824690e13ce0
DENNIS <dennismwangi137@gmail.com>
2023-01-25 14:56:00 +0300

    change nav style

--- script/script.js
+++ script/script.js
@@ -1,8 +1,22 @@
+const openBtn = document.querySelector('.openbtn');
+const closeBtn = document.querySelector('.closebtn');
 
-document.querySelector('.openbtn').addEventListener('click', () => {
+openBtn.addEventListener('click', () => {
     document.getElementById('mySidenav').style.width = "250px";
 }, false);
 
-document.querySelector('.closebtn').addEventListener('click', () => {
+closeBtn.addEventListener('click', () => {
     document.getElementById('mySidenav').style.width = "0";
 }, false);
+
+
+const calc = document.querySelector('#sum');
+
+calc.addEventListener('click', () => {
+    let height = parseFloat(document.querySelector('#height').value);
+    let weight = parseFloat(document.querySelector('#weight').value);
+    let bmi = (weight/height/height) * 10000;
+    document.querySelector('#result').innerHTML = bmi.toFixed(1);
+}, false);
+
+const submit = document.querySelector('#submit');
